f5dcfb03ce05cad15105c07b4ccd6750e64a5287
hoangtuanqn <phamhoangtuanqn@gmail.com>
2025-03-09 04:29:33 +0700

    Optimize feature add to cart

--- src/buyer/view_add_to_cart.c
+++ src/buyer/view_add_to_cart.c
@@ -15,6 +15,8 @@ void view_add_to_cart() {
         msg_error("Currently, there are no products available. Add to cart is not possible.\n");
         return;
     }
+
+    // Get product ID
     do {
         printf("Enter the product id to add to cart: ");
         scanf("%d", &id_product);
@@ -23,6 +25,8 @@ void view_add_to_cart() {
             printf("%d!\n", id_product);
         }
     } while (id_product <= 0 || id_product > counter_product_all);
+
+    // Get quantity
     int quantity_in_stock = product_data[id_product - 1].quantity;
     int quantity_product;
     do {
@@ -36,5 +40,42 @@ void view_add_to_cart() {
             msg_error("products left!\n");
         }
     } while (quantity_product <= 0 || quantity_product > quantity_in_stock);
+
+    // Get cart index for current user
+    int cart_index = current_user.id_cart;
+
+    // Check if product already exists in cart
+    int product_exists = 0;
+    for(int i = 0; i < cart_data[cart_index].quantity; i++) {
+        if(cart_data[cart_index].id_product[i] == id_product) {
+            // Update quantity if doesn't exceed stock
+            int new_quantity = cart_data[cart_index].quantity_product[i] + quantity_product;
+            if(new_quantity > quantity_in_stock) {
+                msg_error("Cannot add more. Total quantity would exceed stock!\n");
+                return;
+            }
+            cart_data[cart_index].quantity_product[i] = new_quantity;
+            product_exists = 1;
+            break;
+        }
+    }
+
+    // If product doesn't exist in cart, add it
+    if(!product_exists) {
+        if(cart_data[cart_index].quantity >= MAX_PRODUCTS) {
+            msg_error("Cart is full! Cannot add more products.\n");
+            return;
+        }
+        
+        // Add new product to cart
+        cart_data[cart_index].id_product[cart_data[cart_index].quantity] = id_product;
+        cart_data[cart_index].quantity_product[cart_data[cart_index].quantity] = quantity_product;
+        cart_data[cart_index].quantity++;
+    }
+
+    // Save cart data to file
+    write_cart_data();
+
+    msg_success("Product added to cart successfully!\n\n");
 
 }
