d7cbd800aa625e56b920ae78520094c2421433d9
hoangtuanqn <phamhoangtuanqn@gmail.com>
2025-03-18 02:20:08 +0700

    Fix add category

--- src/seller/view_add_category.c
+++ src/seller/view_add_category.c
@@ -6,47 +6,57 @@
 #include "../../includes/seller/function_seller.h"
 #include "../../includes/seller/view_add_category.h"
 
-int is_exist_category(char *category_name) {
+int is_exist_category(char *category_name)
+{
     read_category_data();
 
-    for(int i = 0; i < counter_category_all; ++i) {
-        if(strcmp(category_data[i].username, current_user.username) == 0 &&
-            strcmp(category_data[i].category, category_name) == 0) {
+    for (int i = 0; i < counter_category_all; ++i)
+    {
+        if (strcmp(category_data[i].username, current_user.username) == 0 &&
+            strcmp(category_data[i].category, category_name) == 0)
+        {
             return 1;
         }
     }
     return 0;
 }
 
-int handle_add_category(char *category_name) {
-    if(strlen(category_name) == 0) {
+int handle_add_category(char *category_name)
+{
+    if (strlen(category_name) == 0)
+    {
         msg_error("Category name cannot be empty!\n\n");
         return 0;
     }
-    if (is_exist_category(category_name)) {
+    if (is_exist_category(category_name))
+    {
         msg_error("Category already exists!\n\n");
         return 0;
     }
     FILE *file = fopen("data/categories.txt", "a");
-    if (file == NULL) {
+    if (file == NULL)
+    {
         msg_error("Error opening file for appending!\n");
         return 0;
     }
-    fprintf(file, "%s\n%s\n", current_user.username, category_name); // Write username and category name
+    fprintf(file, "%s\n%s\n\n", current_user.username, category_name); // Write username and category name
     fclose(file);
     msg_success("Category added successfully!\n\n");
     return 1;
 }
 
-void view_add_category() {
+void view_add_category()
+{
     char category_name[100];
     int is_success = 0;
-    do {
+    do
+    {
         printf("\n========== Add Category ==========\n");
-        do {
+        do
+        {
             printf("Enter category name: ");
             fgets(category_name, sizeof(category_name), stdin);
-        } while(!input_string(category_name));
+        } while (!input_string(category_name));
         // printf("%s--%d\n", category_name, strlen(category_name));
         is_success = handle_add_category(category_name);
     } while (!is_success);
